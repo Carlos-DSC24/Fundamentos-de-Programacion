866b8338e2006ecfa2c4431b354f5671735a47c0
Carlos Daniel Sanchez Canul <expdaniel19@gmail.com>
2026-09-14 18:56:15 -0600

    Add files via upload

---
+++ Actividades en Clases/Unidad 2/Ejemplos_Practica7/Ejercicio7_MC.java
@@ -0,0 +1,33 @@
+package Ejemplos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio7_MC {
+    public static void main(String args[]) throws IOException {
+        int mes;
+        String salida;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Introduce un numero del 1 al 12:");
+        mes = Integer.parseInt(entrada.readLine());
+        
+        if (mes == 1) { salida = "Enero"; }
+        else if (mes == 2) { salida = "Febrero"; }
+        else if (mes == 3) { salida = "Marzo"; }
+        else if (mes == 4) { salida = "Abril"; }
+        else if (mes == 5) { salida = "Mayo"; }
+        else if (mes == 6) { salida = "Junio"; }
+        else if (mes == 7) { salida = "Julio"; }
+        else if (mes == 8) { salida = "Agosto"; }
+        else if (mes == 9) { salida = "Septiembre"; }
+        else if (mes == 10) { salida = "Octubre"; }
+        else if (mes == 11) { salida = "Noviembre"; }
+        else if (mes == 12) { salida = "Diciembre"; }
+        else { salida = "Mes no válido"; }
+        
+        System.out.println(salida);
+        System.exit(0);
+    }
+}
---
+++ Actividades en Clases/Unidad 2/a2261330035_Practica7/ejercicio_8_2_mc_scanner.java
@@ -0,0 +1,29 @@
+package a2261330035_Practica7;
+
+import java.util.Scanner;
+
+public class ejercicio_8_2_mc_scanner {
+
+	public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)) {
+			double l1, l2, l3;
+
+			System.out.println("Introduce el lado 1:");
+			l1 = entrada.nextDouble();
+			System.out.println("Introduce el lado 2:");
+			l2 = entrada.nextDouble();
+			System.out.println("Introduce el lado 3:");
+			l3 = entrada.nextDouble();
+
+			if ((l1 == l2) && (l2 == l3)) {
+			    System.out.println("Es un triángulo EQUILÁTERO");
+			}
+			if ((l1 != l2) && (l1 != l3) && (l2 != l3)) {
+			    System.out.println("Es un triángulo ESCALENO");
+			}
+			if (((l1 == l2) && (l1 != l3)) || ((l1 == l3) && (l1 != l2)) || ((l2 == l3) && (l2 != l1))) {
+			    System.out.println("Es un triángulo ISÓSCELES");
+			}
+		}
+    }
+}
