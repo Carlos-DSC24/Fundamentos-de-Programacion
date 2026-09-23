43c4b8330dc6f193c2eaee777f373bd0d5c1bb3e
Carlos Daniel Sanchez Canul <a2261330035@alumnos.uat.edu.mx>
2026-09-23 17:26:06 -0600

    Add files via upload

---
+++ Practicas/practicas1/practica1.java
@@ -0,0 +1,9 @@
+package practicas1;
+
+public class practica1 {
+
+	public static void main(String[] args) {
+        System.out.println("Hola Mundo, este es mi primer programa en Java");
+        System.out.println("Materia: Fundamentos de Programacion");
+    }
+}
