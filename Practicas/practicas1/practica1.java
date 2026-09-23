af558355d61c4b59d3f50ca1acda8deb2c72fe22
Carlos Daniel Sanchez Canul <expdaniel19@gmail.com>
2026-09-23 17:27:04 -0600

    Delete Practicas/practicas1 directory

--- Practicas/practicas1/practica1.java
+++
@@ -1,9 +0,0 @@
-package practicas1;
-
-public class practica1 {
-
-	public static void main(String[] args) {
-        System.out.println("Hola Mundo, este es mi primer programa en Java");
-        System.out.println("Materia: Fundamentos de Programacion");
-    }
-}
